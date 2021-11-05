09c4acbb5142961e765e7d312724453b64226521
Andrew Sykes <andrewsykes@btinternet.com>
2021-11-05 16:42:10 +0000

    search html php

--- templates/search.html.php
+++ templates/search.html.php
@@ -1,40 +1,40 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/uploads/includes/helpers.inc.php'; ?>
-
 <h1>Search Files</h1>
 <form action="?" method="get" name="searchFiles">
-<p>View files satisfying the following criteria:</p>
-<?php if(!isset($zero)) :?>
-<div><label for="user">By user: </label>
-<select id="user" name="user">
-<option value="">Any User</option>
-<?php if($priv == "Admin"): ?><optgroup label="clients">
-<?php endif; ?>
-<?php  foreach ($client as $k => $v): ?>
-<option value="<?php htmlout($k); ?>"><?php htmlout($v); ?>
-</option><?php endforeach; ?></optgroup>
-<?php if($priv == "Admin"): ?><optgroup label="users">
-<?php endif; ?>
-<?php  foreach ($users as $k => $v): ?>
-<option value="<?php htmlout($k); ?>"><?php htmlout($v); ?>
-</option><?php endforeach; ?></optgroup></select>
-</div>
-<?php endif; ?>
-<div>
-<label for="text">Containing text:</label> <input id="text" type="search" name="text"/></div>
-<div>
-<div>
-<label for="suffix">Suffix: </label>
-<select id="suffix" name="suffix">
-<option value="">Search files</option>
-<option value="pdf">pdf</option>
-<option value="zip">zip</option>
-<option value="owt">other</option>
-</select></div>
-<input type="hidden" name="action" value="search"/>
-<!--<input type="hidden" name="flag" />-->
-<input type="submit" value="Search"/></div>
-		</form>
-		<p><a href=".">Return</a></p>
-		<!--</div>	-->	
-	</body>
+    <p>View files satisfying the following criteria:</p>
+    <?php if(!isset($zero)) :?>
+    <div><label for="user">By user: </label>
+        <select id="user" name="user">
+            <option value="">Any User</option>
+            <?php 
+            echo $doOpt('clients');
+            foreach ($client as $k => $v){
+                echo $doSelected($k, $v);
+            }
+            echo $doOptEnd();
+            echo $doOpt('users');
+            foreach ($users as $k => $v) {
+                echo $doSelected($k, $v);
+            }
+            echo $doOptEnd(); ?>
+        </select>
+    </div>
+    <?php endif; ?>
+    <div>
+        <label for="text">Containing text:</label> <input id="text" type="search" name="text"/></div>
+    <div>
+        <div>
+            <label for="suffix">Suffix: </label>
+            <select id="suffix" name="suffix">
+                <option value="">Search files</option>
+                <option value="pdf">pdf</option>
+                <option value="zip">zip</option>
+                <option value="owt">other</option>
+            </select></div>
+        <input type="hidden" name="action" value="search"/>
+        <input type="submit" value="Search"/></div>
+	
+</form>
+<p><a href=".">Return</a></p>
+</body>
 </html>
