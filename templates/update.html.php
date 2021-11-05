665625287bc980dbe4e2a38c5dde7145a16e3245
Andrew Sykes <andrewsykes@btinternet.com>
2021-11-05 14:29:54 +0000

    simplify select menus tidy

--- templates/update.html.php
+++ templates/update.html.php
@@ -1,40 +1,34 @@
 <form action="." method="post" name="updateFileInfo">
 <?php
     if($_REQUEST['swap'] == 'No'): ?>
-	<div>
-		<label for="filename">Name: <input id="filename" type="text" name="filename" value="<?php htmlout($filename); ?>"/></label></div>
-	<div>
-		<label for="description">Description: <input id="description" type="text" name="description" value="<?php htmlout($diz); ?>"/></label></div>
-	<div>
-	<?php
-        endif; 
-        //allows Admin to associate a single USER to a single file, USER may/may not belong to a client
-if(($extent < 1) && ($priv === 'Admin')) : ?>
-<div><label for="user">User:&nbsp;</label>
-<select id="user" name="user">
-<?php
-    $update = true;
-    endif;
-    if($extent > 1) : ?>
-    <?php if(isset($answer) && $answer == 'Yes') : ?>
-    <p>Select checkbox to re-assign all client files to selected user. Leave unchecked to simply swap all instances of original owner to selected user</p>
-    <p><label for="blanket">swap/re-assign</label><input type = "checkbox" name="blanket" id="blanket"></p>
-    <?php endif; ?>
-		<div><label for="colleagues">Colleagues:&nbsp;</label>
-            <select id="colleagues" name="colleagues">
-		<?php $update = true;
-                endif;
-                if(isset($update)): ?>
-                <option value="">Select one</option>
-                <?php
-                foreach($colleagues as $k => $v){
-                    echo $doSelected($k, $v);//htmlout messes up here
-                } ?>
+	<div><label for="filename">Name: <input id="filename" type="text" name="filename" value="<?php htmlout($filename); ?>"/></label></div><div><label for="description">Description: <input id="description" type="text" name="description" value="<?php htmlout($diz); ?>"/></label></div><div>
+	<?php endif; 
+    //allows Admin to associate a single USER to a single file, USER may/may not belong to a client
+    if(($extent < 1) && ($priv === 'Admin')) : ?>
+    <div><label for="user">User:&nbsp;</label>
+        <select id="user" name="user">
+            <?php
+            $update = true;
+            endif;
+            if($extent > 1) : 
+            if(isset($answer) && $answer == 'Yes') : ?>
+            <p>Select checkbox to re-assign all client files to selected user. Leave unchecked to simply swap all instances of original owner to selected user</p>
+            <p><label for="blanket">swap/re-assign</label><input type = "checkbox" name="blanket" id="blanket"></p>
+            <?php endif; ?>
+            <div><label for="colleagues">Colleagues:&nbsp;</label>
+                <select id="colleagues" name="colleagues">
+                    <?php $update = true;
+                    endif;
+                    if(isset($update)): ?>
+                    <option value="">Select one</option>
+                    <?php
+                    foreach($colleagues as $k => $v){
+                        echo $doSelected($k, $v);//htmlout messes up here
+                    } ?>
                 </select></div>
-                <?php endif; ?>
-            
-	<div>
-		<input type="hidden" name="fileid" value="<?php htmlout($id); ?>"/>
-		<input type="hidden" name="answer" value="<?php htmlout($answer); ?>"/>
-		<input type="hidden" name="update" value="<?php htmlout($userid); ?>"/>
-        <input type="submit" value="<?php htmlout($button); ?>"/></div></form>
+            <?php endif; ?>
+            <div>
+                <input type="hidden" name="fileid" value="<?php htmlout($id); ?>"/>
+                <input type="hidden" name="answer" value="<?php htmlout($answer); ?>"/>
+                <input type="hidden" name="update" value="<?php htmlout($userid); ?>"/>
+                <input type="submit" value="<?php htmlout($button); ?>"/></div></form>
