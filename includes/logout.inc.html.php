c9059a2756c90cc421ad9ff33a09a8bffc24b2e5
Andrew Sykes <andrewsykes@btinternet.com>
2021-11-25 15:00:18 +0000

    landing page only

--- includes/logout.inc.html.php
+++
@@ -1,5 +0,0 @@
-<form action="" method="post" name="logoutform" class="log">
-<input type="hidden" name="action" value="logout"/>
-    <input type="hidden" name="goto" value="<?php $_SERVER['DOCUMENT_ROOT']?>/uploads/"/>
-    <input type="submit" value="logout"/>
-</form>
